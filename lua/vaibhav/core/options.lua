02fed49b1441c7728ca425728bcea734c324eb89
vaibhav nayak <vaibhavnayak018@gmail.com>
2023-11-11 01:45:59 +0530

    todo comments plugins added

--- lua/vaibhav/core/options.lua
+++ lua/vaibhav/core/options.lua
@@ -11,13 +11,10 @@ opt.shiftwidth = 2
 opt.smartindent = true
 opt.scrolloff = 8
 
-opt.guicursor = ""
+-- opt.guicursor = ""
 
 opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
