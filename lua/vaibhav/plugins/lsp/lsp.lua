94ea6e768f24157143f80be9b60681ed7c16833b
vaibhavm18 <vaibhavnayak018@gmail.com>
2025-08-19 20:35:26 +0530

    context plugin

--- lua/vaibhav/plugins/lsp/lsp.lua
+++ lua/vaibhav/plugins/lsp/lsp.lua
@@ -193,9 +193,18 @@ return {
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        -- clangd = {},
-        -- gopls = {},
+    local servers = {
+      -- clangd = {},
+      gopls = {
+          cmd = { 'gopls' },
+          filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
+        analyses = {
+          unusedparams = true,
+          
+        },
+        staticcheck = true,
+        gofumpt = true,
+      },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
