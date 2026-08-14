b5a0cf3446aefc0c3682f6df17fd25e15db6877a
Vaibhav Nayak <vaibhavnayak018@gmail.com>
2026-08-14 18:24:12 +0530

    small change

--- dotfiles/.fzf.bash
+++
@@ -1,7 +0,0 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */home/vaibhav18/AppImages/fzf-0.65.1/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/vaibhav18/AppImages/fzf-0.65.1/bin"
-fi
-
-eval "$(fzf --bash)"
--- dotfiles/yazi/index.ts
+++
@@ -1,3 +0,0 @@
-let num: number = 10;
-
-console.log(num);
